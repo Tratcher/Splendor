aae5bf6a60c1b7dfd876382facf2db51f5c9b7f4
Chris R <tratcher@outlook.com>
2018-02-05 22:22:27 -0800

    Load and display cards

--- Splendor.Engine/Card.cs
+++ Splendor.Engine/Card.cs
@@ -6,6 +6,15 @@ namespace Splendor.Engine
 {
     public class Card
     {
+        internal Card(string id, int level, IReadOnlyDictionary<GemType, int> cost, GemType bonus, int pointValue)
+        {
+            Id = id;
+            Level = level;
+            Cost = cost;
+            Bonus = bonus;
+            PointValue = pointValue;
+        }
+
         public string Id { get; }
 
         public int Level { get; }
--- Splendor/Program.cs
+++ Splendor/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using Splendor.Engine;
 
 namespace Splendor
 {
@@ -7,6 +9,24 @@ namespace Splendor
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var loader = new CardLoader();
+            var cards = loader.LoadCards("../Splendor.Engine/Resources/Splendor_Card_Table.csv");
+            Console.WriteLine($"Loaded {cards.Count} cards");
+            foreach (var card in cards)
+            {
+                Console.WriteLine(ShowCard(card));
+            }
+        }
+
+        private static string ShowCard(Card card)
+        {
+            var builder = new StringBuilder($"ID: {card.Id}, Level: {card.Level}, Points: {card.PointValue}, Bonus: {card.Bonus}, Cost: ");
+            foreach (var item in card.Cost)
+            {
+                builder.Append($"{item.Key}={item.Value}, ");
+            }
+            return builder.ToString();
         }
     }
 }
